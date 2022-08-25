216af627445d23bc61a7d49e007844c9e387bf76
seosum <ssm347574248@gmail.com>
2022-08-25 16:37:11 +0900

    20220825 test

--- src/main/java/Test/Hello2.java
+++ src/main/java/Test/Hello2.java
@@ -4,5 +4,9 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 	System.out.println("이것이 두번째 테스트이다.");
-	}
+	System.out.println("찐막 테스트!!!");
+	System.out.println("ddd");
+	System.out.println("dddd");
+		}
+
 }
