4b39885b24bdec473e4f351eeaf488a84a7f7092
seosum <ssm347574248@gmail.com>
2022-09-02 17:29:51 +0900

    20220902 배열-나머지

--- src/main/java/BaekJoon/Arrange/EX_3052_나머지.java
+++ src/main/java/BaekJoon/Arrange/EX_3052_나머지.java
@@ -5,20 +5,32 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class EX_3052_나머지 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception {
 		
-		boolean[] arr = new boolean[42];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int i=0; i <10; i++) {
-			arr[Integer.parseInt(br.readLine()) % 42] = true;
+		boolean [] arr = new boolean [42]; 
+		// 나머지값이기 때문에 boolean으로 값을 지정해준다
+		// 나머지는 0-41이기떄문에 42로 배열을 지정해준다
+		// boolean의 초기값은 false이다.
 		
-	}
 		int count = 0;
-		for(boolean value : arr) {
-			if(value) {
-				count++;
+		// true로 변한 배열값을 세기위한 변수임.
+		
+		for(int i=0; i< arr.length; i++) {
+			// 배열을 0으로 초기화 시켜준다.
+
+			arr[Integer.parseInt(br.readLine())%42] = true;
+			// 입력받은 값의 나머지에 해당하는 인덱스를 true로 바꿔준다.
 			}
+			
+				for(boolean value : arr) {
+					// 향상된 for문 
+					if(value) {
+						count++;
+					}
+				}
+				
+				System.out.println(count); 
 		}
-	}
 }
